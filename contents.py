1a9c118dc59d6d1ea7ec25c36fb3df6683742da9
adrie <1750927@uab.cat>
2026-04-28 23:21:41 +0200

    Created load_contents() function for Movie class

--- contents.py
+++ contents.py
@@ -1,10 +1,62 @@
 from abc import ABC, abstractmethod
+import pandas as pd
+import re
 
 class Content (ABC):
-    pass
+    
+    @abstractmethod
+    def load_content(self):
+        pass
 
 class Movie (Content):
-    pass
+    def __init__(self):
+        #_id: int
+        #genres: str
+        #released: int
+        #rating: float
+        #timestamp: str
+        self.content_dict: dict[str, dict] = {}
+    
+    @staticmethod
+    def _extract_info(row):
+        title_full = row['title']
+        year_match = re.search(r'\((\d{4})\)$', title_full)
+        
+        if year_match:
+            year = int(year_match.group(1))
+            # Remove the " (YYYY)" part from the title string
+            clean_title = re.sub(r'\s\(\d{4}\)$', '', title_full)
+        else:
+            year = None
+            clean_title = title_full
+        
+        return clean_title, year
+    def load_content(self):
+        movies_df = pd.read_csv("./books/Books.csv")
+        
+        with open("./books/Books.csv", "r") as books_file:
+            pass
+        # Apply extraction to create new columns
+        movies_df[['title', 'release_year']] = movies_df.apply(
+            lambda x: pd.Series(self.extract_info(x)), axis=1
+        )
+        
+        # 3. Convert genres string into a list of strings
+        movies_df['genres'] = movies_df['genres'].str.split('|')
+        
+        # 4. Set movieId as the index and convert to a nested dictionary
+        # 'index' orientation creates the {index: {column: value}} structure you need
+        content_dict = movies_df.set_index('movieId').to_dict(orient='index')
+
 
 class Book (Content):
-    pass
+    def __init__(self):
+        #ISBN: str
+        #title: str
+        #author: str
+        #publication_year: int
+        #publisher: str
+        self.content_dict: dict[str, dict] = {}
+    
+    def load_content(self):
+        pass
---
+++ main.py
@@ -0,0 +1,20 @@
+from contents import Movie, Book
+from ratings import SimpleRating, CollaborativeRating
+
+chosen_db = input("Do you want to view movies (m) or books (b)?\n")
+if chosen_db == "m":
+    content = Movie()
+elif chosen_db == "b":
+    content = Book()
+else:
+    raise ValueError
+content.load_content()
+
+chosen_ratings = input("Which rating method do you prefer? (simple s, collaborative c)\n")
+if chosen_ratings == "s":
+    pass
+elif chosen_ratings == "c":
+    pass
+else:
+    raise ValueError
+
