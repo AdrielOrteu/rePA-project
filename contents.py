84f68f22fc4dcfef9a5fbbb129e4819107e2f666
adrie <1750927@uab.cat>
2026-04-28 19:28:38 +0200

    Created initial files

---
+++ contents.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+
+class Content (ABC):
+    pass
+
+class Movie (Content):
+    pass
+
+class Book (Content):
+    pass
---
+++ ratings.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+
+class Rating (ABC):
+    pass
+
+class SimpleRating (Rating):
+    pass
+
+class CollaborativeRating (Rating):
+    pass
